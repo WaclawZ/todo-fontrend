1088f97ec1cbb2d5dc9341ea738a69fc77b83495
WaclawZ <waclaw.zalicki@gmail.com>
2020-03-24 23:35:22 +0100

    run eslint on files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const App = () =>  {
-  return (
-    <div>
-      New app
-    </div>
-  );
-};
+const App = () => (
+  <div>
+    New app
+  </div>
+);
 
 export default App;
